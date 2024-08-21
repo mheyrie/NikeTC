35f49c16942885808e1d77fda0e8e86839053b0b
Mheyrie <omota4fidelity@gmail.com>
2024-08-21 01:27:54 +0100

    Creating files

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+
+const Home = () => (
+    <>
+    </>
+);
+
+export default Home;
