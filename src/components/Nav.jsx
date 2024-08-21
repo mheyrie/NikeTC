766d5155f69599a1ec9939bc1bcd756bc4b724b4
Mheyrie <omota4fidelity@gmail.com>
2024-08-21 04:14:30 +0100

    Hamburger styling ongoing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { headerLogo } from "../assets/images";
-import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleHamburger = () => {
+    setOpen((prev) => !prev);
+  };
+  const handleNavLinkClick = () => {
+    setOpen(false);
+  };
   return (
     <header className="padding-x py-8 z-10 absolute w-full">
       <nav className="flex justify-between items-center max-container">
@@ -11,18 +18,50 @@ const Nav = () => {
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center item-center gap-16 max-lg:hidden">
-        {navLinks.map((item) =>(
+          {navLinks.map((item) => (
             <li key={item.label}>
-                <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
-                    {item.label}
-                </a>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
             </li>
-        ))}
+          ))}
         </ul>
-        <div className="hidden max-lag:block">
-            
+        {/* Hamburger Icon  */}
+        <div>
+          {/* <div className="hidden max-lag:block"> */}
+          <button
+            id="menu-btn"
+            onClick={handleHamburger}
+            className={`block hamburger lg:hidden ${open ? "open" : ""}`}
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
         </div>
       </nav>
+      {open ? (
+        <div className="bg-red-300">
+            <ul className="flex flex-col my-10 justify-start space-y-6 font-bold left-2 right-2">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a
+                    href={item.href}
+                    className="font-montserrat leading-normal text-lg text-slate-gray"
+                    onClick={handleNavLinkClick}
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+        </div>
+      ) : (
+        ""
+      )}
     </header>
   );
 };
