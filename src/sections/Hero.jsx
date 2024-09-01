13c6d47430da99392d1ead4f495aedd9263c131a
Mheyrie <omota4fidelity@gmail.com>
2024-09-01 16:30:49 +0100

    Right Hersection

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import {arrowRight} from '../assets/icons'
+import { statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -8,15 +9,22 @@ const Hero = () => {
       className="w-full flex flex-col xl:flex-row justify-center min-h-screen max-container gap-10"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collections</p>
-        <h1>
-          <span> The New Arrival</span>
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap z-10 pr-10"> The New Arrival</span>
           <br />
-          <span>Nike</span> Shoes
+          <span className="text-coral-red mt-3 inline-block">Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike Shoes arrival, quality comfort and innovation that suits whatever lifestyle you have</p>
+        <p className="font-montserrat text-slate-600 text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike Shoes arrival, quality comfort and innovation that suits whatever lifestyle you have</p>
         <Button label={"Shop Now"} iconURL={arrowRight}/>
-        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16"></div>
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index)=>(
+            <div key={index}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="font-montserrat leading-7 text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
