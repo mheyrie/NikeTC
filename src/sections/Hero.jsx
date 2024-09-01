8dfc3898e64d3eda1c6917aa8ee0a1421c1fbc25
Mheyrie <omota4fidelity@gmail.com>
2024-09-01 19:49:59 +0100

    ShoeCard component created

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import Button from "../components/Button";
 import {arrowRight} from '../assets/icons'
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   return (
@@ -12,7 +13,7 @@ const Hero = () => {
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap z-10 pr-10"> The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap z-10 pr-10 relative"> The New Arrival</span>
           <br />
           <span className="text-coral-red mt-3 inline-block">Nike</span> Shoes
         </h1>
@@ -27,8 +28,16 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        {/* <div className="relative"> */}
         <img src={bigShoe1} alt="Shoe Collection" width={600} height={500} className="object-contain z-10 relative" />
+        <div>
+          {shoes.map((shoe, index)=>(
+            <div key={index}>
+              <ShoeCard />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
