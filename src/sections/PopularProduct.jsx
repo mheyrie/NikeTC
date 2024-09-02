74bbb06ffdf5c9e39d1bff6e48a7abe55f9e444e
Mheyrie <omota4fidelity@gmail.com>
2024-09-02 23:27:05 +0100

    Working on populareprodutcs

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,9 +1,16 @@
-
+import { products } from "../constants";
 
 const PopularProduct = () => {
   return (
-    <div>PopularProduct</div>
-  )
-}
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span>  Product</h2>
+        <p className="">
+          Experience top notch quality and styles with a sort after collections
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default PopularProduct
+export default PopularProduct;
