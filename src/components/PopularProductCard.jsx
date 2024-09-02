50fcfe4f11b18423b67fb3a7981f6482264aa015
Mheyrie <omota4fidelity@gmail.com>
2024-09-03 00:09:44 +0100

    Popular product card updated

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -6,6 +6,7 @@ const PopularProductCard = ({ imgURL, name, price }) => {
     <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
     <div className="mt-8 flex justify-start gap-2 5">
         <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
     </div>
   </div>;
 };
