210e6ea80b0696e5fa13ad794e55aeef6b957593
Mheyrie <omota4fidelity@gmail.com>
2024-09-02 22:35:47 +0100

    Working on left side hero

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+
+  const handleClick = ()=>{
+    if(bigShoeImg !== imgURL.bigShoe){
+      changeBigShoeImage(imgURL.bigShoe)
+    }
+  }
   return (
-    <div className="rounded-xl border-2 ">ShoeCard</div>
+    <div className={`rounded-xl border-2 ${bigShoeImg === imgURL? 'border-coral-red':'border-transparent'} cursor-pointer max-sm:flex-1`} onClick={handleClick}>
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img src={imgURL.thumbnail} alt="" width={127} height={103} className="object-contain"/>
+      </div>
+    </div>
   )
 }
 
