5f6468d22c1fe01c67352d67ef5d33953192d4ca
Mheyrie <omota4fidelity@gmail.com>
2024-09-03 00:03:55 +0100

    Created PopularProductCard

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PopularProductCard = ({product}) => {
+  return (
+    <div>PopularProductCard</div>
+  )
+}
+
+export default PopularProductCard
--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,3 +1,4 @@
+import PopularProductCard from "../components/PopularProductCard";
 import { products } from "../constants";
 
 const PopularProduct = () => {
@@ -5,9 +6,14 @@ const PopularProduct = () => {
     <section id="products" className="max-container max-sm:mt-12">
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span>  Product</h2>
-        <p className="">
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top notch quality and styles with a sort after collections
         </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap gap-14">
+        {products.map((product,index)=>(
+          <PopularProductCard key={index}  {...product}/ >
+        ))}
       </div>
     </section>
   );
