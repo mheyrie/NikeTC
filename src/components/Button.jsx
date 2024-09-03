a4c0ca1193c21cbc73ab567e577ff192ff447c30
Mheyrie <omota4fidelity@gmail.com>
2024-09-03 04:27:51 +0100

    SpecialOffer Scetion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 p-3 text-lg border font-montserrat leading-none bg-coral-red rounded-full text-white border-coral-red">
-      {label}
-      {iconURL && (
-        <img
-          src={iconURL}
-          alt="Arrow Right Icon"
-          className="ml-2 rounded-full w-5 h-5"
-        />
-      )}
-    </button>
+    <button
+    className={`flex justify-center items-center gap-2 p-3 text-lg border font-montserrat leading-none 
+      ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : "bg-coral-red text-white border-coral-red"}
+    rounded-full`}
+  >
+    {label}
+    {iconURL && (
+      <img
+        src={iconURL}
+        alt="Arrow Right Icon"
+        className="ml-2 rounded-full w-5 h-5"
+      />
+    )}
+  </button>
+  
   );
 };
 
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,9 +1,32 @@
-
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
 
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
-  )
-}
+    <section className="flex justify-wrap items-center max-xl:flex-col-revers gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer}  alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl  capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red"> Special </span>
+         Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio quo nam facilis voluptatum, accusamus deleniti reiciendis beatae laboriosam, dolore fugit deserunt modi. Maxime nobis necessitatibus libero, dolores deleniti quaerat voluptate.
+          
+        </p>
+        <p className="mt-4 lg:max-w-lg info-text">
+        Officiis, reiciendis. Eveniet recusandae vero, consequatur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel consequuntur. 
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label={"Shop Now"} iconURL={arrowRight}/>
+          <Button label={"Learn More"} backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffers
+export default SpecialOffers;
