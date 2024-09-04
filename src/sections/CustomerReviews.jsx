2a10f42f52f782fb7f70ffd76b1da4abedd46a96
Mheyrie <omota4fidelity@gmail.com>
2024-09-04 01:16:00 +0100

    Created ReviewCard

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,16 @@
+import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
     <section className="max-container">
       <h3 className="font-palanquin text-4xl font-bold text-center"> What Our
-      <span> Customers </span>Says?</h3>
+      <span className="text-coral-red"> Customers </span>Says?</h3>
+      <p className="info-text max-w-lg text-center m-auto mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque earum suscipit molestiae optio vitae ut magnam dolorem.</p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:lex-col gap-14">
+        {reviews.map((review, index)=>(
+          <ReviewCard/>
+        ))}
+      </div>
     </section>
   )
 }
