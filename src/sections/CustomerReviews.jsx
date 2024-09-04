478480a52c2207c626f764b309c0302b3bf6176f
Mheyrie <omota4fidelity@gmail.com>
2024-09-04 05:06:59 +0100

    Review card styling updated

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -7,7 +7,7 @@ const CustomerReviews = () => {
       <h3 className="font-palanquin text-4xl font-bold text-center"> What Our
       <span className="text-coral-red"> Customers </span>Says?</h3>
       <p className="info-text max-w-lg text-center m-auto mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque earum suscipit molestiae optio vitae ut magnam dolorem.</p>
-      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:lex-col gap-14">
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review, index)=>(
           <ReviewCard key={index} review={review} imgURL={review.imgURL}customerName={review.customerName} rating={review.rating} feedback={review.feedback} />
         ))}
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -7,7 +7,7 @@ const Subscribe = () => {
       <h3 className="text-4xl leading-[68px] font-bold lg:mx-w-md font-palanquin">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="Subscribe@nike.cm" className="input" />
-        <div className="fex max-sm justify-end items-center max-sm:w-full">
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label={"Sign Up"} fullWidth/>
         </div>
       </div>
