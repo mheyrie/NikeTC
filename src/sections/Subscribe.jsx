ff4d35ff751b8b5e3477bcf3597a3883a7e6dc98
Mheyrie <omota4fidelity@gmail.com>
2024-09-04 02:01:38 +0100

    Subscribe

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -5,7 +5,7 @@ const Subscribe = () => {
   return (
     <section id="contact-us" className="max-container flex justify-between items-center max-lg:flex-col gap-10">
       <h3 className="text-4xl leading-[68px] font-bold lg:mx-w-md font-palanquin">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
-      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate">
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="Subscribe@nike.cm" className="input" />
         <div className="fex max-sm justify-end items-center max-sm:w-full">
           <Button label={"Sign Up"} fullWidth/>
