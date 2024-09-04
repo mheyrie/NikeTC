70025b27f961ca5120a2771ed389405e7c59a461
Mheyrie <omota4fidelity@gmail.com>
2024-09-04 01:33:28 +0100

    working on Subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,7 +2,9 @@
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section id="contact-us" className="max-container flex justify-between items-center max-lg:flex-col gap-10">
+      <h3 className="text-4xl leading-[68px] font-bold lg:mx-w-md font-palanquin">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
+    </section>
   )
 }
 
