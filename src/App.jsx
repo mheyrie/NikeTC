007ae0ad8d156ae9227ca0b018c3c54720da71da
Mheyrie <omota4fidelity@gmail.com>
2024-09-08 19:13:06 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Homes from "./pages/Homescreen/Homes";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductMain from "./pages/Product/ProductMain";
 
 const App = () => (
