e7e4d40c3f8db84a34b70616db3663c630bf0f2f
Mheyrie <omota4fidelity@gmail.com>
2024-09-09 23:18:28 +0100

    Updated

--- src/pages/Product/ProductMain.jsx
+++ src/pages/Product/ProductMain.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProductMain = () => {
   return (
-    <div className="text-red-600 " >
-      <div className="">
-        <h1 c>Hell</h1>
-        <h1>Helloo</h1>
+    <div className="text-red-600 relative" >
+      <div className="pt-80">
+        <h1>Hell</h1>
+        <h1>Helloooo</h1>
         <h1>Helloo</h1>
         <h1>Helloo</h1>
         <h1>Helloo</h1>
