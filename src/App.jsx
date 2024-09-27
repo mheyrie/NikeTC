f06dbdf0c3e49cb4628bd6cc21d05eeae9ecdc43
Mheyrie <omota4fidelity@gmail.com>
2024-09-27 16:37:30 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import Nav from "./components/Nav";
 import Homes from "./pages/Homescreen/Homes";
 import ProductMain from "./pages/Product/ProductMain";
 import useDarkMode from "./hooks/useDarkMode";
+import { useState } from "react";
 
-const App = () => (
-  <Router>
-    <main className="relative">
-      <Nav />
-      <button className="p-2 bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
-        Toggle{colorTheme === "dark" ? "Light" : "Dark"} Mode
-      </button>
-      <Routes>
-        <Route path="/" element={<Homes />} />
-        <Route path="products" element={<ProductMain />} />
-      </Routes>
-    </main>
-  </Router>
-);
+const App = () => {
+  const [colorTheme, setTheme] = useDarkMode();
 
+  return (
+    <Router>
+      <main className="relative min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        <Nav  />
+        
+        <Routes>
+          <Route path="/" element={<Homes />} />
+          <Route path="products" element={<ProductMain />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
 export default App;
