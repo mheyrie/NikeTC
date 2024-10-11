c050251ad66d631c75c7b372b0f0c52cf0d934fc
Mheyrie <omota4fidelity@gmail.com>
2024-10-11 10:11:27 +0100

    Fixing stylying in dark mode

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -4,7 +4,7 @@ import { reviews } from "../constants"
 const CustomerReviews = () => {
   return (
     <section className="max-container">
-      <h3 className="font-palanquin text-4xl font-bold text-center"> What Our
+      <h3 className="font-palanquin text-4xl font-bold text-center dark:text-gray"> What Our
       <span className="text-coral-red"> Customers </span>Says?</h3>
       <p className="info-text max-w-lg text-center m-auto mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque earum suscipit molestiae optio vitae ut magnam dolorem.</p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
