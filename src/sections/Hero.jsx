0f88fc37bc060c3d88a461a76b3749311d8b45bd
Mheyrie <omota4fidelity@gmail.com>
2024-10-11 10:47:52 +0100

    updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,6 @@ const Hero = () => {
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white dark:bg-slate-600 dark:flex-1 xl:whitespace-nowrap z-10 pr-10 relative">
-            {" "}
             The New Arrival
           </span>
           <br />
